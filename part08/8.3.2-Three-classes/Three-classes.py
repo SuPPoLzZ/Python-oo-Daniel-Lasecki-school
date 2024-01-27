ecdcb513a0efe7c55b115738f3626702495405fa
edu.przemyslaw.lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2024-01-27 18:21:55 +0200

    three classes

---
+++ part08/8.3.2-Three-classes/Three-classes.py
@@ -0,0 +1,16 @@
+class Checklist:
+    def __init__(self, attribute_header: str, attribute_entries: list):
+        self.attribute_header = attribute_header
+        self.attribute_entries = attribute_entries
+
+class Custormer:
+    def __init__(self, attribute_id: str, attribute_balance: str, attribute_discount: int):
+        self.attribute_id = attribute_id
+        self.attribute_balance = attribute_balance
+        self.atrribute_discount = attribute_discount
+        
+        
+class Cable:
+    def __init__(self, attribute_model: str, attribute_length: str, attribute_max_speed: float, attribute_bidirectional: bool):
+        s
+    

0691a2504f27cfd76bfc4300f18c0b68a0b5ba7f
edu.przemyslaw.lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2024-02-21 17:08:51 +0200

    lunchcard 2.0

--- part08/8.5.3-LunchCard/lunchcard.py
+++ part08/8.5.3-LunchCard/lunchcard.py
@@ -6,6 +6,9 @@ class LunchCard:
         if self.balance > 0:
             self.balance -= 2.60
         
+    def deposit_money(self, amount: float):
+        self.balance += amount
+    
     def eat_special(self):
          if self.balance > 0:
             self.balance -= 6.60
@@ -17,14 +20,27 @@ class LunchCard:
         return f"The balance is {finalbalance} Euro"
     
     
-    
+peters_card = LunchCard(20)
+graces_card = LunchCard(30)
+peters_card.eat_special()
+graces_card.eat_lunch()
+print(f"........................................................................")
+print(f"Balance after Grace eats normal lunch and Peter eats special lunch")
+print(f"Peter balance {peters_card}")
+print(f"Graces balance {graces_card}")
+peters_card.deposit_money(20)
+graces_card.eat_special()
+print(f"........................................................................")
+print(f"Balance after Grace eats special lunch and Peter deposits 20")
+print(f"Peter balance {peters_card}")
+print(f"Graces balance {graces_card}")
+peters_card.eat_lunch()
+peters_card.eat_lunch()
+graces_card.deposit_money(50)
+print(f"........................................................................")
+print(f"Balance after Peter eats 2x lunch and Grace deposits 50")
+print(f"Peter balance {peters_card}")
+print(f"Graces balance {graces_card}")
+print(f"........................................................................")
 
-card = LunchCard(2.62)
-print(card)
 
-card.eat_lunch()
-print(card)
-
-card.eat_special()
-card.eat_lunch()
-print(card)

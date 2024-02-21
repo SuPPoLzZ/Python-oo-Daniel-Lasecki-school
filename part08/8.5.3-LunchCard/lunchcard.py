ed6b4e6b2294b4f9d49d995217cf01ff7507604b
edu.przemyslaw.lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2024-02-21 15:54:17 +0200

    lunchcard.py

---
+++ part08/8.5.3-LunchCard/lunchcard.py
@@ -0,0 +1,29 @@
+class LunchCard:
+    def __init__(self, balance: float):
+        self.balance = balance
+    
+    def eat_lunch(self):
+        if self.balance > 0:
+            self.balance -= 2.60
+        
+    def eat_special(self):
+         if self.balance > 0:
+            self.balance -= 6.60
+
+    def __str__(self):
+        if self.balance < 0 or self.balance ==  0:
+                return f"inssuficient balance try again later"
+        return f"The balance is {self.balance}"
+    
+    
+    
+
+card = LunchCard(2.62)
+print(card)
+
+card.eat_lunch()
+print(card)
+
+card.eat_special()
+card.eat_lunch()
+print(card)

abee13a85f05431bc90649b7edd591023869f6d0
edu.przemyslaw.lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2024-02-27 14:13:20 +0200

    27.2.2024

--- part09/9.1.4-LunchCard-and-PaymentTerminal/lunchcard_and_paymentTerminal.py
+++ part09/9.1.4-LunchCard-and-PaymentTerminal/lunchcard_and_paymentTerminal.py
@@ -18,6 +18,30 @@ class LunchCard:
         # Otherwise it returns False.
 
 
+class PaymentTerminal:
+    def __init__(self):
+        # Initially there is 1000 euros in cash available at the terminal
+        self.funds = 1000
+        self.lunches = 0
+        self.specials = 0
+
+    def eat_lunch(self, payment: float):
+
+        # A regular lunch costs 2.50 euros.
+        # Increase the value of the funds at the terminal by the 
+        # price of the lunch, increase the number of lunches sold, 
+        # and return the appropriate change.
+        # If the payment passed as an argument is not large enough to cover
+        # the price, the lunch is not sold, and the entire sum is returned.
+
+    def eat_special(self, payment: float):
+        # A special lunch costs 4.30 euros.
+        # Increase the value of the funds at the terminal by the 
+        # price of the lunch, increase the number of lunches sold, 
+        # and return the appropriate change.
+        # If the payment passed as an argument is not large enough to cover
+        # the price, the lunch is not sold, and the entire sum is returned.
+
 
 if __name__ == "__main__":
     card = LunchCard(10)
@@ -28,3 +52,18 @@ if __name__ == "__main__":
     result = card.subtract_from_balance(4)
     print("Payment successful:", result)
     print("Balance", card.balance)
+
+exactum = PaymentTerminal()
+
+change = exactum.eat_lunch(10)
+print("The change returned was", change)
+
+change = exactum.eat_lunch(5)
+print("The change returned was", change)
+
+change = exactum.eat_special(4.3)
+print("The change returned was", change)
+
+print("Funds available at the terminal:", exactum.funds)
+print("Regular lunches sold:", exactum.lunches)
+print("Special lunches sold:", exactum.specials)

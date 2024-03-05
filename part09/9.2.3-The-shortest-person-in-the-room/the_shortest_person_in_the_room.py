018cb59db34f74daa24c1817dc68b203f6c15f37
edu.przemyslaw.lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2024-03-05 14:02:09 +0200

    05.03.2024

--- part09/9.2.3-The-shortest-person-in-the-room/the_shortest_person_in_the_room.py
+++ part09/9.2.3-The-shortest-person-in-the-room/the_shortest_person_in_the_room.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, name:str, height: int):
+    def __init__(self, name: str, height: int):
         self.name = name
         self.height = height
 
@@ -7,7 +7,6 @@ class Person:
 class Room:
     def __init__(self):
         self.persons = []
-    
 
     def add(self, person: Person):
         self.persons.append(person)
@@ -19,22 +18,34 @@ class Room:
         if self.is_empty():
             print("The room is empty")
         else:
-            total_height = sum(person.height for person in self.persons)  # Calculate the total height of persons
+            total_height = sum(person.height for person in self.persons)
             print(f"There are {len(self.persons)} persons in the room, and their combined height is {total_height} cm")
             for person in self.persons:
                 print(f"{person.name} ({person.height} cm)")
 
+    def shortest(self):
+        if self.persons:
+            shortest_person = min(self.persons, key=lambda x: x.height)
+            return shortest_person.height  # Return the height of the shortest person
+        else:
+            return None
 
 
-
-
-    
 room = Room()
+
 print("Is the room empty?", room.is_empty())
+print("Shortest:", room.shortest())
+
 room.add(Person("Lea", 183))
 room.add(Person("Kenya", 172))
-room.add(Person("Ally", 166))
 room.add(Person("Nina", 162))
-room.add(Person("Dorothy", 155))
+room.add(Person("Ally", 166))
+
+print()
+
 print("Is the room empty?", room.is_empty())
+print("Shortest:", room.shortest())
+
+print()
+
 room.print_contents()
